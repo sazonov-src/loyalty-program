3b71835b2ac68840d883312d2da3a2f8683610e1
Vitalii Sazonov <vitaliy89vin@gmail.com>
2024-10-02 12:39:49 +0300

    add shadcn ui, the structure of react components has been changed

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,41 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { generateClient } from "aws-amplify/data";
 import { type Schema } from '../amplify/data/resource'
-import { useState, useEffect } from 'react';
+import { 
+  useState, useEffect,
+  createContext, 
+  useContext,
+} from 'react';
+import { Button } from '@/components/ui/button';
+import { Separator } from "@/components/ui/separator"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
-const userId = "59a0ecad-9939-4c7d-a6d3-aaae198a4fbe";
+const userId = "fdd53ed9-d0ee-4ccf-a72e-8178e009df83";
+const wrighOff = [5, 7, 10];
+const client = generateClient<Schema>();
 
-export default function App() {
-  const client = generateClient<Schema>();
-  const [ bonuses, setBonuses ] = useState<number>(0);
+interface StateContextType {
+  value: number;
+  setValue: (value: number) => void;
+}
 
+const BonusesContext = createContext<StateContextType | undefined>(undefined);
+
+function SetBonusButton() {
+  const context = useContext(BonusesContext);
+  if (!context) throw new Error("useTheme must be used within a ThemeProvider");
+  const {setValue: setBonuses} = context;
+  
   async function setBonus() {
     const { data: result, errors: error } = await client
       .mutations.AddBonus({
@@ -20,21 +47,47 @@ export default function App() {
     console.log(result, error)
   };
 
-  async function getBonuses() {
-    const { data: customer, errors: error } =  await client
-      .models.User.get({ 
-        id: userId,
-      });
-    if (!customer) return;
-    setBonuses(customer.bonusPoints ?? 0);
-    console.log(customer, error)
-  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button
+          variant="default"
+          size="lg"
+        >
+          Set Bonus
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={setBonus}
+          >Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
 
+function WriteOffBonusesButton( { 
+  count, 
+}: { count: number } ) {
+  const context = useContext(BonusesContext);
+  if (!context) throw new Error("useTheme must be used within a ThemeProvider");
+  const {setValue: setBonuses} = context;
+  
   async function writeOffBonuses() {
     const { data: customer, errors: error } = await client
       .mutations.WriteOffBonuses({
         userId: userId,
-        decrement: 5
+        decrement: count
       })
     if (!customer) {
       alert("not enough bonuses");
@@ -44,21 +97,68 @@ export default function App() {
     console.log(customer, error)
   };
 
+  return (
+    <Button
+      onClick={writeOffBonuses}
+      variant="outline"
+      size="sm"
+      className="mb-4"
+    >
+      Write Off {count} Bonuses
+    </Button>
+  );
+}
+
+
+export default function App() {
+
+  const [ bonuses, setBonuses ] = useState<number>(0);
+
   useEffect(() => {
     getBonuses();
   }, []);
 
+  async function getBonuses() {
+    const { data: customer } = await client
+      .models.User.get({ 
+        id: userId,
+      });
+    if (!customer) return;
+    setBonuses(customer.bonusPoints ?? 0);
+  }
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <h2>Bonuses: { bonuses }</h2>
-          <button onClick={setBonus}>Set Bonus</button>
-          <button onClick={writeOffBonuses}>Write Off Bonuses</button>
-          <a onClick={signOut}>Sign out</a>
-        </main>
-      )}
-    </Authenticator>
+    <BonusesContext.Provider value={{ value: bonuses, setValue: setBonuses }}>
+      <div className="flex flex-col items-center justify-center h-screen"> 
+      <Authenticator>
+        {({ signOut }) => (
+          <>
+          <h1 className="text-3xl font-bold mb-6">
+            Бонуси клієнта { userId }
+          </h1>
+
+          <div className="text-2xl mb-6">
+            Поточні бонуси: { bonuses }
+          </div>
+
+          <SetBonusButton />
+
+          <Separator className="my-6" />
+
+          <div className="space-x-4 mb-16">
+            { wrighOff.map((num) => (
+              <WriteOffBonusesButton count={ num } />
+            ))}
+          </div>
+
+          <Button onClick={ signOut } variant='link' >
+            Вихід
+          </Button>
+          </>
+        )}
+      </Authenticator>
+    </div>      
+    </BonusesContext.Provider>
   );
 };
+
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Amplify } from 'aws-amplify';
 import outputs from '../amplify_outputs.json';
+import { ThemeProvider } from "@/components/theme-provider"
 import App from './App.tsx'
 import './index.css'
 
@@ -9,6 +10,8 @@ Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
+  </ThemeProvider>
   </StrictMode>,
 )
